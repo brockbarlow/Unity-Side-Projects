0597709006bbe49703a15cdbb61d08313cc58e81
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-05-12 16:01:58 -0500

    Instantiation Test Commit
    tested to see if the player and objects could be instantiated. Test was
    successful.

---
+++ First Person Camera/Assets/Scripts/Player Three/PrefabClone.cs
@@ -0,0 +1,25 @@
+﻿namespace Player_Three
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PrefabClone : MonoBehaviour
+    {
+
+        public GameObject playerPrefabObject;
+        public GameObject groundPrefabObject;
+        public GameObject airPrefabObject;
+
+        private GameObject m_PlayerPrefabObjectClone;
+        private GameObject m_GroundPrefabObjectClone;
+        private GameObject m_AirPrefabObjectClone;
+
+        void Start()
+        {
+            m_PlayerPrefabObjectClone = Instantiate(playerPrefabObject, transform.position, Quaternion.identity);
+            m_GroundPrefabObjectClone = Instantiate(groundPrefabObject, transform.position, Quaternion.identity);
+            m_AirPrefabObjectClone = Instantiate(airPrefabObject, transform.position, Quaternion.identity);
+        }
+    }
+}
