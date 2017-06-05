3e20389ca0f43b8df2b04f9377bd5e2300409d3c
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-06-05 17:38:05 -0500

    Added Project Commit
    added count down timer project.

---
+++ Count Down Timer/Assets/Scripts/timer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//using delta time
+
+public class timer : MonoBehaviour
+{
+    public float countdownValue = 10;
+    public float countdown;
+
+    void Start()
+    {
+        countdown = countdownValue;
+        StartCoroutine(StartCountdown());
+    }
+
+    public IEnumerator StartCountdown()
+    {
+        while (countdown >= 0)
+        {
+            Debug.Log(countdown);
+            yield return null;
+            countdown -= Time.deltaTime;
+        }
+
+        if (countdown < 0)
+        {
+            countdown = 0f;
+        }
+    }
+}
+
+//----------------------------------------------------------//
+//without using delta time
+
+//public class timer : MonoBehaviour
+//{
+//    public float countdownValue = 10;
+//    public float countdown;
+
+//    void Start()
+//    {
+//        StartCoroutine(StartCountdown());
+//    }
+
+//    public IEnumerator StartCountdown()
+//    {
+//        countdown = countdownValue;
+
+//        while (countdown >= 0)
+//        {
+//            Debug.Log(countdown);
+//            yield return new WaitForSeconds(1.0f);
+//            countdown--;
+//        }
+//    }
+//}
+//----------------------------------------------------------//
